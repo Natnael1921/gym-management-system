36ce5e701e07ae3a13c66f4b1431e51fc5a12b5d
Natnael Mekonnen <your_nnatnaelmekonnen19.com>
2025-12-09 23:51:47 +0300

    setup server and connect Database

---
+++ Backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongoDB connected successfully !!");
+  } catch (error) {
+    console.log("mongoDB connection error", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
